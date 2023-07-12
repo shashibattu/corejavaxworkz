c117c8a9cbb6bcde7783c7e84ab4566c12726d99
battushashi1 <battushashidevops@gmail.com>
2023-07-12 14:06:19 +0530

    adding the movie.java file to the repo corejavaxworkz

---
+++ Movies.java
@@ -0,0 +1,69 @@
+class Movies{
+
+static String kannadaMovie[]={"kgf","milana","villain","robert","kantara","rangi taranga","lucia","myna","ugram"};
+static String hindiMovie[]={"jab we met","dabang","pk","sholey","bahubali 1","bahubali 2","lagaan","gullu boy"};
+static String englishMovie[]={"The Godfather","Pulp Fiction","The Dark Knight","Fight Club","Forrest Gump"};
+static String teluguMovie[]={"Arjun Reddy","Rangasthalam","Ala Vaikunthapurramuloo","Mahanati","Srimanthudu","Fidaa"};
+static String tamilMovie[]={"Mersal","oke oka jeevitham","Kaithi","Vikram Vedha","Vada Chennai","Pariyerum Perumal"};	
+
+public static void main (String args[]){
+
+k();
+l();
+m();
+n();
+o();i
+
+}
+public static void k(){
+	System.out.println("\n");
+System.out.println("kannada movies names");
+
+for(String a: kannadaMovie){
+System.out.println(a);
+	
+}
+
+}
+public static void l(){
+	System.out.println("\n");
+System.out.println("hindi movies names");
+
+for(String b: hindiMovie){
+System.out.println(b);
+	
+}
+
+}
+public static void m(){
+	System.out.println("\n");
+System.out.println("english movies names");
+
+for(String c: englishMovie){
+System.out.println(c);
+	
+}
+
+}
+public static void n(){
+	System.out.println("\n");
+System.out.println("Telugu movies names");
+
+for(String d: teluguMovie){
+System.out.println(d);
+	
+}
+
+}
+public static void o(){
+	System.out.println("\n");
+System.out.println("Tamil movies names");
+
+for(String e: tamilMovie){
+System.out.println(e);
+	
+}
+
+}
+
+}
